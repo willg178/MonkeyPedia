cb19b226741df50d6e340c0c494f4da9772e9c7c
Will Grund <willgrund178@gmail.com>
2022-05-12 16:27:05 -0400

    a couple things

---
+++ MonkeyPedia/monkey-front/src/views/MonkeyIndex.vue
@@ -0,0 +1,41 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      monkeys: [],
+      currentMonkey: {},
+      //   monkeyFilter: "",
+    };
+  },
+
+  created: function () {
+    axios.get("http://localhost:3000/monkeys.json").then((response) => {
+      this.monkeys = response.data;
+      console.log("All Monkeys");
+    });
+  },
+};
+</script>
+
+<template>
+  <div class="row">
+    <div v-for="monkey in monkeys" v-bind:key="monkey.id" class="col-sm-3 mb-4">
+      <div class="card" style="width: 18rem">
+        <img
+          src="monkey.image_url"
+          class="card-img-top"
+          alt="no monke pic :,("
+        />
+        <div class="card-body">
+          <h5 class="card-title">{{ monkey.name }}</h5>
+          <p class="card-text">
+            {{ monkey.description }}
+          </p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
