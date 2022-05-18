f9470805bad5e2e5a0db0b58bab194be4dacc5ab
Will Grund <100606734+willg178@users.noreply.github.com>
2022-05-18 17:46:32 -0400

    Deletes old repository

--- Actualize/blog-frontend/src/views/BEEGView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="about">
-    <h1>BEEG YOSHI</h1>
-    <img
-      src="https://preview.redd.it/aoh4z6fwold21.png?auto=webp&s=235ccc0f2b4b68b909d18dea301fb828e4e79451"
-    />
-    <h1>He's sittin' there</h1>
-  </div>
-</template>
